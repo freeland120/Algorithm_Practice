cb029345208510d324ee6f0019fa4a738da0a32f
freeland120 <freeland120@naver.com>
2020-06-08 02:42:47 +0900

    2020.6.8 Solving_Algorithm

--- CodingTest/Test_Even_Odd.java
+++ CodingTest/Test_Even_Odd.java
@@ -2,27 +2,24 @@ package CodingTest;
 
 import java.util.Scanner;
 
-
 public class Test_Even_Odd {
 
 	public String Even_Odd(int num) {
-		
+
 		String result = "";
 		int check = num % 2;
-		
-		if(check==0) {
-			result="Even";
-		}else {
-			result="Odd";
+
+		if (check == 0) {
+			result = "Even";
+		} else {
+			result = "Odd";
 		}
 		return result;
-		
+
 	}
-	
+
 	public static void main(String[] args) {
-		
-		
-		
+
 	}
-	
+
 }
--- CodingTest/Test_Stack.java
+++ CodingTest/Test_Stack.java
@@ -15,28 +15,28 @@ class Stack<T> {
 	private Node<T> top;
 
 	public T pop() {
-		if(top == null) {
+		if (top == null) {
 			throw new EmptyStackException();
 		}
-		
+
 		T item = top.data;
 		top = top.next;
 		return item;
 	}
-	
+
 	public void push(T item) {
 		Node<T> t = new Node<T>(item);
 		t.next = top;
 		top = t;
 	}
-	
+
 	public T peek() {
-		if(top == null) {
+		if (top == null) {
 			throw new EmptyStackException();
 		}
 		return top.data;
 	}
-	
+
 	public boolean isEmpty() {
 		return top == null;
 	}
@@ -60,7 +60,7 @@ public class Test_Stack {
 		System.out.println(s.pop());
 		System.out.println(s.pop());
 		System.out.println(s.isEmpty());
-		
+
 	}
 
 }
