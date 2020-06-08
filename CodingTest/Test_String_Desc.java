7ff10764dda1477d08bd5546632d97945a333bec
freeland120 <freeland120@naver.com>
2020-06-08 13:19:06 +0900

    2020.6.8 Notes

--- CodingTest/Test_String_Desc.java
+++ CodingTest/Test_String_Desc.java
@@ -8,19 +8,18 @@ public class Test_String_Desc {
 
 	public String string_desc(String s) {
 		String answer = "";
-		
+
 		String str[] = s.split("");
 		Arrays.sort(str);
 		Collections.reverse(Arrays.asList(str));
-		
+
 		return String.join("", str);
 	}
-	
-	
+
 	public static void main(String[] args) {
-		
+
 		Scanner input = new Scanner(System.in);
-		
+
 		Test_String_Desc c = new Test_String_Desc();
 		System.out.println(c.string_desc("Zbcdefg"));
 	}
