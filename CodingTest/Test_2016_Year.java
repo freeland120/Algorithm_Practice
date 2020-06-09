34d77479bc84833a40850ba3507793539e92585d
freeland120 <freeland120@naver.com>
2020-06-09 17:49:09 +0900

    2020.6.9 Solving_Algorithm28

---
+++ CodingTest/Test_2016_Year.java
@@ -0,0 +1,19 @@
+package CodingTest;
+
+import java.util.Scanner;
+
+public class Test_2016_Year {
+
+	
+	
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		Test_2016_Year c = new Test_2016_Year();
+		
+		
+		
+		
+	}
+
+}
