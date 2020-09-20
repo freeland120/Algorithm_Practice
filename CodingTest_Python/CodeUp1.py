0efb2024556a37520a2bde5852cca609466db07a
DeokGyeong Yu <freeland120@naver.com>
2020-09-21 05:45:56 +0900

    2020.9.21 Notes

---
+++ CodingTest_Python/CodeUp1.py
@@ -0,0 +1,51 @@
+# #기초100문제
+
+# #1
+# print("Hello")
+
+# #2
+# print("Hello World")
+
+# #3
+# print("Hello\nWorld")
+
+# #4
+# print("\'Hello\'")
+
+# #5
+# print("\"Hello World\"")
+
+# #6
+# print("\"!@#$%^&*()\"")
+
+# #7
+# print("\"C:\\Download\\hello.cpp\"")
+
+# #8
+# print("\u250C\u252C\u2510\n\u251C\u253C\u2524\n\u2514\u2534\u2518")
+
+# #9
+# n = int(input())
+
+# print(n)
+
+# #10
+# char = str(input())
+# print(char)
+
+# char2 = input()
+# print(char2)
+
+# #11
+# n = float(input())
+# print(format(n,".6f"))
+
+# #12
+# a, b = map(int,input().split())
+# print(a,b)
+
+# #13
+# a, b = map(str,input().split())
+# print(b,a)
+
+
