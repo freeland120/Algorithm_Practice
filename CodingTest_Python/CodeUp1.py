3028b8cbb7f354fe67833bf30c3e0649e8aa10cf
DeokGyeong Yu <freeland120@naver.com>
2020-09-21 23:28:50 +0900

    2020.9.21 Notes

--- CodingTest_Python/CodeUp1.py
+++ CodingTest_Python/CodeUp1.py
@@ -1,51 +1,118 @@
 # #기초100문제
 
-# #1
+# #1001
 # print("Hello")
 
-# #2
+# #1002
 # print("Hello World")
 
-# #3
+# #1003
 # print("Hello\nWorld")
 
-# #4
+# #1004
 # print("\'Hello\'")
 
-# #5
+# #1005
 # print("\"Hello World\"")
 
-# #6
+# #1006
 # print("\"!@#$%^&*()\"")
 
-# #7
+# #1007
 # print("\"C:\\Download\\hello.cpp\"")
 
-# #8
+# #1008
 # print("\u250C\u252C\u2510\n\u251C\u253C\u2524\n\u2514\u2534\u2518")
 
-# #9
+# #1010
 # n = int(input())
 
 # print(n)
 
-# #10
+# #1011
 # char = str(input())
 # print(char)
 
 # char2 = input()
 # print(char2)
 
-# #11
+# #1012
 # n = float(input())
 # print(format(n,".6f"))
 
-# #12
+# #1013
 # a, b = map(int,input().split())
 # print(a,b)
 
-# #13
+# #1014
 # a, b = map(str,input().split())
 # print(b,a)
 
+# #1015
+# n = float(input())
+# print(format(round(n,3),".2f"))   
 
+# round(n,3)
+# result2 = format(n,".2f")
+# print(result2)
+
+# #1017
+# n = int(input())
+# for i in range(3):
+#     print(n,end=' ')
+
+# #1018
+# hour, minute = input().split(':')
+# print(hour + ":" + minute)
+
+
+##1019
+
+# from datetime import datetime
+
+# year, month, day = input().split(".")
+
+# print("%04d" %int(year), end ='.')
+# print("%02d" %int(month), end='.')
+# print("%02d" %int(day))
+
+# from datetime import datetime
+
+# year, month, day = input().split(".")
+
+# Year=year.zfill(4)
+# Month=month.zfill(2)
+# Day=day.zfill(2)
+
+# print(Year+"."+Month+"."+Day)
+
+##1020
+# a = "5000"
+
+# print(a.zfill(5))     //zfill(width) 함수 사용
+# print(a.rjust(5,"a"))     //rjust(width, [fillchar]) 함수 사용
+
+# Birthday, etc = input().split("-")
+
+# print(Birthday + etc)
+
+
+##1021
+# Char = str(input())
+
+# print(Char)
+
+
+##1022
+# String = str(input())
+
+# print(String)
+
+
+##1023
+# int_number, float_number = input().split(".")
+
+# print(int_number)
+# print(float_number)
+
+##1024
