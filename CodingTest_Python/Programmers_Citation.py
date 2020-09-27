e067a0e02db9b39a06f7747f8fd1674bddeefd87
DeokGyeong Yu <freeland120@naver.com>
2020-09-27 19:35:59 +0900

    2020.9.27 Notes

--- CodingTest_Python/Programmers_Citation.py
+++ CodingTest_Python/Programmers_Citation.py
@@ -2,19 +2,19 @@
 
 
 
-def solution(citations):
-    citations.sort()
+# def solution(citations):
+#     citations.sort()
 
-    l = len(citations)
+#     l = len(citations)
     
-    for i in range(l):
-        if citations[i] >= l-i:
-        # 논문이 인용된 횟수(h번 이상) >= 인용된 논문의 개수(h개 == h번)
-            return l - i
-    return 0
+#     for i in range(l):
+#         if citations[i] >= l-i:
+#         # 논문이 인용된 횟수(h번 이상) >= 인용된 논문의 개수(h개 == h번)
+#             return l - i
+#     return 0
 
 
 
-citations = [3,0,6,1,6]
+# citations = [3,0,6,1,6]
 
-print(solution(citations))
+# print(solution(citations))
