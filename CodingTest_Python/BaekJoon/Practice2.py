b64757c8b1d8bf9251233bb847441d920172c9ea
DeokGyeong Yu <freeland120@naver.com>
2020-09-30 21:53:27 +0900

    2020.9.30 Notes?

--- CodingTest_Python/BaekJoon/Practice2.py
+++ CodingTest_Python/BaekJoon/Practice2.py
@@ -1,7 +1,7 @@
 
 
 import math
-
+import time
 
 
 def isPrime(N):
@@ -18,21 +18,60 @@ def countPrimes(N):
     for i in range(2,N+1):
         if isPrime(i):
             count += 1
-            print(i,"는 소수입니다.")
+            
     return count
+
+
+
+def countPrimes2(N):
+    sieve = [False,False] + [True] * (N-1)
+    primes = []
+    count2 = 0
+    for i in range(2,N+1):
+        primes.append(i)
+    
+        for j in range(i * 2, N+1, i):
+            sieve[j] = False
+
+    #print(sieve)
+
+    for k in range(2,N+1):
+        if sieve[k]:
+            count2 += 1       
+
+    return count2
+
 
 
 N = int(input())
 
-result = isPrime(N)
+# result = isPrime(N)
 
-if result == True:
-    print(N,"은 소수입니다❗❗❗")
+# if result == True:
+#     print(N,"은 소수입니다❗❗❗")
 
-else:
-    print(N,"은 소수가 아닙니다❗❗❗")
+# else:
+#     print(N,"은 소수가 아닙니다❗❗❗")
 
 
+countPrimes_time_start = time.time()
 result2 = countPrimes(N)
-
+countPrimes_elapsed = time.time() - countPrimes_time_start
 print(N,"까지의 소수의 갯수는:",result2,"개 입니다.")
+print(countPrimes_elapsed, '초')
+print(countPrimes_elapsed/60, '분')
+print(countPrimes_elapsed/60/60, '시간')
+
+
+
+countPrimes2_time_start = time.time()
+result3 = countPrimes2(N)
+countPrimes2_elapsed = time.time() - countPrimes2_time_start
+print(N,"까지의 소수는:",result3,"개 입니다.")
+print(countPrimes2_elapsed, '초')
+print(countPrimes2_elapsed/60, '분')
+print(countPrimes2_elapsed/60/60, '시간')
+
+
+
+
